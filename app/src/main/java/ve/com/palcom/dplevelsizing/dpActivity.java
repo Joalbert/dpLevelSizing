dc14cb57944172070ada47ba488f7ec0d18e6e38
joalbert <joalbertp@gmail.com>
2017-05-06 19:41:37 -0400

    Version 1.0.1.3

--- app/src/main/java/ve/com/palcom/dplevelsizing/dpActivity.java
+++ app/src/main/java/ve/com/palcom/dplevelsizing/dpActivity.java
@@ -1,13 +1,10 @@
 package ve.com.palcom.dplevelsizing;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
